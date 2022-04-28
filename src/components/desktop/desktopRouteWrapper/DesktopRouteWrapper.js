df57f6a45b17096d9db4dd63cc0e64e4821a2888
unknown <sawarz22@gmail.com>
2022-04-28 23:46:48 +0200

    Add DesktopRouteWrapper

---
+++ src/components/desktop/desktopRouteWrapper/DesktopRouteWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.css'
+import ToolBar from '../toolBar/ToolBar'
+
+export default function DesktopRouteWrapper(props) {
+  return (
+    <div className={styles.desktopPage}>
+      <ToolBar ></ToolBar>
+      <div className={styles.desktopPageContent}></div>
+          {props.component}
+    </div>
+  )
+}
