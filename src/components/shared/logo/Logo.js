b926b2de4298ac20c67dfe4b0537cc38ef6387ff
unknown <sawarz22@gmail.com>
2022-04-28 23:48:19 +0200

    Add Logo component

---
+++ src/components/shared/logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.css'
+import { Link } from 'react-router-dom'
+import logoImg from '../../../assets/logo.png'
+
+export default function Logo() {
+  return (
+      <Link className={styles.logoLink} to="/">
+          <img src={logoImg} height="80px"></img>
+    </Link>
+  )
+}
