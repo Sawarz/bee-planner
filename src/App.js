7bcb87e633d5f9106925f3027e571646e47d9759
unknown <sawarz22@gmail.com>
2022-04-28 23:44:46 +0200

    Added App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import DesktopRouteWrapper from './components/desktop/desktopRouteWrapper/DesktopRouteWrapper';
+import Dashboard from './components/desktop/dashboard/Dashboard';
+import { Provider } from 'react-redux';
+import Store from './redux/Store';
 
 function App() {
   return (
     <div className="App">
-      
+      <Provider store={Store}>
+        <Routes>
+          <Route path="/" element={<DesktopRouteWrapper component={<Dashboard />}></DesktopRouteWrapper>}></Route>
+        </Routes>
+      </Provider>
     </div>
   );
 }
