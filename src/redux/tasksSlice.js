d3dd00ec61473ffcaf163cdfd97e1bbcfe8f3613
unknown <sawarz22@gmail.com>
2022-05-19 23:51:19 +0200

    Add tasksSlice to redux

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -6,10 +6,21 @@ export const tasksSlice = createSlice({
     reducers: {
         addTask: (state, action) => {
             state.push(action.payload);
+        },
+        deleteTask: (state, action) => {
+            let arrayIndex;
+            console.log(action.payload);
+            for (const [i, task] of state.entries()) {
+                if (action.payload.name === task.name) {
+                    arrayIndex = i;
+                    break;
+                }
+            }
+            state.splice(arrayIndex, 1);
         }
     }
   })
   
-  export const { addTask } = tasksSlice.actions
+  export const { addTask, deleteTask } = tasksSlice.actions
   
   export default tasksSlice.reducer
