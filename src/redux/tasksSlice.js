dbd32f925317fe0796df1c1a0dcd0a54fd52641a
unknown <sawarz22@gmail.com>
2022-05-24 23:37:36 +0200

    Add new reducer

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -6,16 +6,26 @@ export const tasksSlice = createSlice({
     reducers: {
         addTask: (state, action) => {
             state.push(action.payload);
+            return state;
         },
         deleteTask: (state, action) => {
             let arrayIndex = state.findIndex((task) => {
                 return task.name = action.payload.name;
             })
             state.splice(arrayIndex, 1);
+            return state;
+        },
+        updateTaskLeftPosition: (state, action) => {
+            for (let task of state) {
+                if (task.id == action.payload.id) {
+                    task.left = action.payload.left;
+                }
+            }
+            return state;
         }
     }
   })
   
-  export const { addTask, deleteTask } = tasksSlice.actions
+  export const { addTask, deleteTask, updateTaskLeftPosition } = tasksSlice.actions
   
   export default tasksSlice.reducer
