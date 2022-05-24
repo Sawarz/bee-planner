8b7c810f2d81e0b98157267a589875522dc63936
unknown <sawarz22@gmail.com>
2022-05-24 23:51:56 +0200

    Move Store provider to index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter>
+  <HashRouter>
+    <Provider store={store}>
       <App />
-    </HashRouter> 
+    </Provider>
+  </HashRouter> 
 );
 
 // If you want to start measuring performance in your app, pass a function
