26dd9dff3a17a35df9fd91296ab94bd9b2c73d6b
unknown <sawarz22@gmail.com>
2022-06-05 17:25:05 +0200

    Add calendar link and media query layout

--- src/components/desktop/toolBar/ToolBar.js
+++ src/components/desktop/toolBar/ToolBar.js
@@ -4,13 +4,18 @@ import Logo from '../../shared/logo/Logo'
 import AppName from '../../shared/appName/AppName'
 import AddTask from './addTask/AddTask'
 import Profile from './profile/Profile'
+import { Link } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
+import calendarImg from '../../../assets/calendar.png'
 
 export default function ToolBar() {
+  const mobileDevice = useMediaQuery({query: 'max-width:480px'});
     return (
-      <div className={styles.toolBar}>
+      <div className={`${styles.toolBar} ${mobileDevice ? null : styles.mobileToolBar}`}>
         <Logo></Logo>
         <AppName></AppName>
         <AddTask></AddTask>
+        <Link className={styles.calendarLink} to="/calendar"><img alt="calendar" className={styles.calendarImg} src={calendarImg} /></Link>
         <Profile></Profile>
       </div>
   )
