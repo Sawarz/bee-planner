ae01e98a148c027947ab58010c7dc53feb9963db
unknown <sawarz22@gmail.com>
2022-07-10 15:55:32 +0200

    Fixed initial day

--- src/components/desktop/dashboard/Dashboard.js
+++ src/components/desktop/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './styles.module.css'
 import Day from '../day/Day'
 import { useParams } from "react-router-dom";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentDay } from '../../../redux/currentDaySlice';
 
 export default function Dashboard(props) {
@@ -10,7 +10,11 @@ export default function Dashboard(props) {
   const dispatch = useDispatch();
 
   let currentDay;
-  currentDay = params.day;
+  let initialDate = useSelector(state => state.todayDate);
+  if (params.day === undefined)
+    currentDay = initialDate;
+  else
+    currentDay = params.day;
   dispatch(setCurrentDay(currentDay));
 
   return (
