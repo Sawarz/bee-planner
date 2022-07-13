c5d51fd8c92785f709995930a0b896129d8895ef
unknown <sawarz22@gmail.com>
2022-07-13 09:51:50 +0200

    Responsive calendar component

--- src/components/desktop/calendar/Calendar.js
+++ src/components/desktop/calendar/Calendar.js
@@ -2,9 +2,13 @@ import React from 'react'
 import styles from './styles.module.css'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Calendar() {
   let todayDate = useSelector(state => state.todayDate);
+
+  const isTablet = useMediaQuery({ maxWidth: 900 });
+  const isMobile = useMediaQuery({ maxWidth: 500 });
 
   let year = new Date().getFullYear();
   let month = new Date().getMonth()+1;
@@ -61,15 +65,39 @@ export default function Calendar() {
   return (
     <div className={styles.calendar}>
       <div className={styles.todayDate}>{todayDate}</div>
-      <div className={styles.weekdays}>
-        <div className={styles.weekday}>Monday</div>
-        <div className={styles.weekday}>Tuesday</div>
-        <div className={styles.weekday}>Wednesday</div>
-        <div className={styles.weekday}>Thursday</div>
-        <div className={styles.weekday}>Friday</div>
-        <div className={styles.weekday}>Saturday</div>
-        <div className={styles.weekday}>Sunday</div>
-      </div>
+      {isTablet ?
+        isMobile ?
+        <div className={styles.weekdays}>
+          <div className={styles.weekday}>M</div>
+          <div className={styles.weekday}>T</div>
+          <div className={styles.weekday}>W</div>
+          <div className={styles.weekday}>Th</div>
+          <div className={styles.weekday}>F</div>
+          <div className={styles.weekday}>Sa</div>
+          <div className={styles.weekday}>Su</div>
+        </div>
+        :
+        <div className={styles.weekdays}>
+          <div className={styles.weekday}>Mon</div>
+          <div className={styles.weekday}>Tue</div>
+          <div className={styles.weekday}>Wed</div>
+          <div className={styles.weekday}>Thu</div>
+          <div className={styles.weekday}>Fri</div>
+          <div className={styles.weekday}>Sat</div>
+          <div className={styles.weekday}>Sun</div>
+        </div>
+        :
+        <div className={styles.weekdays}>
+          <div className={styles.weekday}>Monday</div>
+          <div className={styles.weekday}>Tuesday</div>
+          <div className={styles.weekday}>Wednesday</div>
+          <div className={styles.weekday}>Thursday</div>
+          <div className={styles.weekday}>Friday</div>
+          <div className={styles.weekday}>Saturday</div>
+          <div className={styles.weekday}>Sunday</div>
+        </div>
+      }
+      
       <div className={styles.month}>
         {previousMonthDays()}
         {currentMonthDays()}
